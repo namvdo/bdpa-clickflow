8f8d9881c30168f3e8c7efaa341aba87f819954b
Leo <leo.davidov@outlook.com>
2026-04-29 01:34:46 +0300

    Bump version to 0.2.0 and update packages
    Updated package version and dependencies.

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="bdpa-clickflow",
-    version="0.1.0",
-    packages=["utils", "pipeline"]
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "scikit-learn"
+    ],
 )
